b490bbfec1dab5a91c867cf3edeabe9dfb8e8d0c
Fangzhou Lu <fangzhoulu@hiretual.com>
2021-05-07 11:56:55 +0800

    Basic websocket example

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/user', function(req, res, next) {
+  console.log(req.body);
+  res.status(200).end();
 });
 
 module.exports = router;
